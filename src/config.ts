ecf51990fad6c0b7c95b4c566dad2fd7a960458f
Growingmindset <94933065+Growingmindset@users.noreply.github.com>
2026-05-11 00:00:41 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://rudygalan.com", 
+  author: "Rudy Galan",
+  profile: "https://rudygalan.com/",
+  desc: "Integrated OS: Systems-oriented builds in Physics, Engineering, and Business Logic.",
+  title: "Rudy Galan",,
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -19,5 +19,5 @@ export const SITE = {
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "America/Chicago", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
